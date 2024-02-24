eebbf87179215aa77a34ce6c1888164a4eba752c
Noelle Jung <noellejung0301@gmail.com>
2024-02-23 18:30:54 -0800

    replaced db password with env variable

--- supafast/database.py
+++ supafast/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres.kiibpnonuwblvbzwowqm:/#RPNMm8p+A9*&F@aws-0-us-west-1.pooler.supabase.com:5432/postgres"
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://postgres.kiibpnonuwblvbzwowqm:{os.getenv(key='DB_PASSWORD')}@aws-0-us-west-1.pooler.supabase.com:5432/postgres"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
